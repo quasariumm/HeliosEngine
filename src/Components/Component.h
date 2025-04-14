62cf7ef280e9e4a578340301b2812af53d9647f1
QuillanW <56843492+QuillanW@users.noreply.github.com>
2025-04-14 11:26:30 +0200

    Scene, scene objects and components
    Uploaded scene, scene objects and component system
    
    FEAT: Scene
    FEAT: Scene objects
    FEAT: Transform
    FEAT: Component System

---
+++ src/Components/Component.h
@@ -0,0 +1,51 @@
+﻿#pragma once
+#include <imgui.h>
+#include <map>
+#include <utility>
+
+namespace Engine
+{
+    class SceneObject;
+
+    /// To create a component, add this struct as a variable to your class. You can then assign that variable to an object.
+    struct Component
+    {
+        Component(const std::type_info& objectType, void* rawComponent, std::string  name ): m_componentType(objectType), m_rawComponent(rawComponent), m_displayName(std::move(name))
+        {
+        }
+
+        void AttachToObject(SceneObject* object) { m_attachedObject = object; }
+        [[nodiscard]] const std::type_info& GetType() const { return m_componentType; }
+        [[nodiscard]] void* GetRawComponent() const { return m_rawComponent; }
+        [[nodiscard]] std::string GetName() const {return m_displayName; }
+        [[nodiscard]] SceneObject* GetAttachedObject() const { return m_attachedObject; }
+        
+        void AssignProperty(const std::string& displayName, int& valueRef) { m_intProperties.insert({ displayName, valueRef }); }
+        void AssignProperty(const std::string& displayName, float& valueRef) { m_floatProperties.insert({ displayName, valueRef }); }
+        void AssignProperty(const std::string& displayName, vec2& valueRef) { m_vec2Properties.insert({ displayName, valueRef }); }
+        void AssignProperty(const std::string& displayName, vec3& valueRef) { m_vec3Properties.insert({ displayName, valueRef }); }
+
+        void DisplayProperties() const
+        {
+            for (std::pair<std::string, int&> p : m_intProperties)
+                ImGui::InputInt(p.first.c_str(), &p.second);
+            for (std::pair<std::string, float&> p : m_floatProperties)
+                ImGui::InputFloat(p.first.c_str(), &p.second);
+            for (std::pair<std::string, vec2&> p : m_vec2Properties)
+                ImGui::InputFloat2(p.first.c_str(), p.second.cell);
+            for (std::pair<std::string, vec3&> p : m_vec3Properties)
+                ImGui::InputFloat3(p.first.c_str(), p.second.cell);
+        }
+        
+    private:
+        SceneObject* m_attachedObject = nullptr;
+        const std::type_info& m_componentType;
+        const std::string m_displayName;
+        void* m_rawComponent;
+
+        std::map<std::string, int&> m_intProperties;
+        std::map<std::string, float&> m_floatProperties;
+        std::map<std::string, vec2&> m_vec2Properties;
+        std::map<std::string, vec3&> m_vec3Properties;
+    };
+}
---
+++ src/Scene/Scene.h
@@ -0,0 +1,88 @@
+﻿#pragma once
+#include "SceneObjects.h"
+
+namespace Engine
+{
+    class Scene
+    {
+    public:
+        /// Generate a new random UID.
+        int NewUID()
+        {
+            int i = 0;
+
+            return (int)m_sceneObjects.size() + 1;
+
+            // while (i < 100)
+            // {
+            //     uint newUID = Rand();
+            //     bool failed = false;
+            //     for (SceneObject* object : m_sceneObjects)
+            //         if (object->GetUID() == newUID && NewUID() != 0)
+            //             failed = true;
+            //
+            //     if (!failed)
+            //         return newUID;
+            //     i++;
+            // }
+            //
+            // std::cout << ANSI_ERR <<
+            //     "Creation of new UID took too long, Try again or remove some objects.\n";
+
+            // return 0;
+        }
+
+        /// Create and empty object for the scene. Returns the newly created object.
+        SceneObject* NewObject()
+        {
+            auto object = new SceneObject(NewUID());
+
+            // Check for valid UID
+            if (object->GetUID() == 0)
+            {
+                delete object;
+                return nullptr;
+            }
+
+            m_sceneObjects.push_back(object);
+            return object;
+        }
+
+        /// Delete an object from the scene, also deletes any child objects.
+        void DeleteObject(int UID)
+        {
+            for (int i = 0; i < (int)m_sceneObjects.size(); i++)
+                if (m_sceneObjects[i]->GetUID() == UID)
+                {
+                    delete m_sceneObjects[i];
+                    m_sceneObjects.erase(m_sceneObjects.begin() + i);
+                }
+        }
+
+        /// Delete an object from the scene, also deletes any child objects.
+        void DeleteObject(SceneObject* object) { DeleteObject(object->GetUID()); }
+
+        /// Find object by UID.
+        [[nodiscard]] SceneObject* GetSceneObject(int UID) const
+        {
+            for (SceneObject* object : m_sceneObjects)
+                if (object->GetUID() == UID)
+                    return object;
+            return nullptr;
+        }
+
+        /// Find object by name.
+        [[nodiscard]] SceneObject* GetSceneObject(const std::string& name) const
+        {
+            for (SceneObject* object : m_sceneObjects)
+                if (object->GetName() == name)
+                    return object;
+            return nullptr;
+        }
+
+        std::vector<SceneObject*>& GetSceneObjectList() { return m_sceneObjects; }
+
+    private:
+        std::vector<SceneObject*> m_sceneObjects = {};
+    };
+}
