caafa5ec64145355675ef5fe69a424f21f1dad59
Patrick Vreeburg <96523159+quasariumm@users.noreply.github.com>
2025-04-15 15:50:31 +0200

    FIX: Linker errors for random functions

--- src/Math/Random.h
+++ src/Math/Random.h
@@ -8,14 +8,14 @@
 namespace Engine
 {
 
-static uint32_t RandomSeed(uint32_t val);
+extern uint32_t RandomSeed(uint32_t val);
 
-static uint32_t RandomUInt(uint32_t& seed, uint32_t min = 0, uint32_t max = UINT_MAX);
+extern uint32_t RandomUInt(uint32_t& seed, uint32_t min = 0, uint32_t max = UINT_MAX);
 
-static float RandomFloat(uint32_t& seed, float min = 0.f, float max = 1.f);
+extern float RandomFloat(uint32_t& seed, float min = 0.f, float max = 1.f);
 
-static vec3f RandomDirection(uint32_t& seed);
+extern vec3f RandomDirection(uint32_t& seed);
 
-static vec2f RandomPointInUnitCircle(uint32_t& seed);
+extern vec2f RandomPointInUnitCircle(uint32_t& seed);
 
 };
