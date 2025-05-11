fb92e956c8f8cbf6da17edf499417cc04dc1ded5
quasariumm <patrickvreeburg@protonmail.com>
2025-05-11 20:20:23 +0200

    FEAT: Proper dielectric material

--- src/Shaders/Raytracing/materials.glsl
+++ src/Shaders/Raytracing/materials.glsl
@@ -9,11 +9,12 @@
 */
 
 #define MATERIAL_REFLECTION 	1
-#define MATERIAL_TRANSMISSION 	2
-#define MATERIAL_DIFFUSE 		4
-#define MATERIAL_GLOSSY 		8
-#define MATERIAL_SPECULAR 		16
-#define MATERIAL_ALL 			MATERIAL_REFLECTION | MATERIAL_TRANSMISSION | MATERIAL_DIFFUSE | MATERIAL_GLOSSY | MATERIAL_SPECULAR
+#define MATERIAL_MICROFACET		2
+#define MATERIAL_TRANSMISSION 	4
+#define MATERIAL_DIFFUSE 		8
+#define MATERIAL_GLOSSY 		16
+#define MATERIAL_SPECULAR 		32
+#define MATERIAL_ALL 			MATERIAL_REFLECTION | MATERIAL_MICROFACET | MATERIAL_TRANSMISSION | MATERIAL_DIFFUSE | MATERIAL_GLOSSY | MATERIAL_SPECULAR
 
 struct RayTracingMaterial
 {
@@ -64,7 +65,7 @@ vec3 Reflect(in vec3 wo, in vec3 normal)
 
 // Returns direction in xyz, Fresnel term in w
 // TODO: Support for refraction from non-air to non-air
-vec4 Refract(in vec3 wo, in vec3 normal, in float etaT, out float outEta)
+vec4 Refract(in vec3 wo, in vec3 normal, in float etaT, out float etaO, out float etaI)
 {
 	float eta = etaT;
 	vec3 N = normal;
@@ -90,7 +91,16 @@ vec4 Refract(in vec3 wo, in vec3 normal, in float etaT, out float outEta)
 	float x = (1.0 - cosThetaI);
 	float fresnel = R0 + (1.0 - R0) * x * x * x * x * x;
 
-	outEta = eta;
+	if (eta == etaT)
+	{
+		etaI = 1.0;
+		etaO = etaT;
+	}
+	else
+	{
+		etaI = etaT;
+		etaO = 1.0;
+	}
 	return vec4(normalize(-wo / eta + (cosThetaI / eta - cosThetaT) * N), fresnel);
 }
 
@@ -98,7 +108,7 @@ vec4 Refract(in vec3 wo, in vec3 normal, in float etaT, out float outEta)
 	BRDF, BSSDF and PDF
 */
 
-struct TypeInfo
+struct BRDFInfo
 {
 	float factor;
 	vec3 direction;
@@ -108,9 +118,9 @@ struct TypeInfo
 // These functions are combined for the reason that the same values need to be calculated eitherway
 vec3 GetBRDFAndBounce(inout RayTracingMaterial material, inout Ray ray, inout uint seed, in RayHitInfo info)
 {
-	TypeInfo diffuse 		= TypeInfo(0.0, vec3(0.0), vec3(0.0));
-	TypeInfo specular 		= TypeInfo(0.0, vec3(0.0), vec3(0.0));
-	TypeInfo transmission 	= TypeInfo(0.0, vec3(0.0), vec3(0.0));
+	BRDFInfo diffuse 		= BRDFInfo(0.0, vec3(0.0), vec3(0.0));
+	BRDFInfo specular 		= BRDFInfo(0.0, vec3(0.0), vec3(0.0));
+	BRDFInfo transmission 	= BRDFInfo(0.0, vec3(0.0), vec3(0.0));
 
 	bool glassReflect = false;
 
@@ -118,17 +128,21 @@ vec3 GetBRDFAndBounce(inout RayTracingMaterial material, inout Ray ray, inout ui
 		&& material.specularity < 1.0)
 	{
 		diffuse.factor = 1.0 - material.specularity;
+		// Direction
 		if ((material.type | MATERIAL_REFLECTION) != 0)
 			diffuse.direction = LambertianBounce(seed, info.normal);
 
+		// BRDF
 		diffuse.BRDF = material.diffuseColor * INVPI;
 	}
 	if ((material.type | MATERIAL_SPECULAR) != 0
 		&& material.specularity > 0.0)
 	{
 		specular.factor = material.specularity;
+		// Direction
 		if ((material.type | MATERIAL_REFLECTION) != 0)
 			specular.direction = Reflect(-ray.dir, info.normal);
+		// BRDF
 		// https://www.cs.cmu.edu/afs/cs/academic/class/15462-f09/www/lec/lec8.pdf
 		vec3 H = normalize(info.lightVector - ray.dir);
 		float overlap = max(0.0, dot(H, info.normal));
@@ -138,42 +152,51 @@ vec3 GetBRDFAndBounce(inout RayTracingMaterial material, inout Ray ray, inout ui
 		&& material.refractivity > 0.0)
 	{
 		transmission.factor = material.refractivity;
-		if (material.refractionCoefficient == 1.0
-			|| ((material.type | MATERIAL_REFLECTION) != 0))
+
+		if ((material.type | MATERIAL_REFLECTION) != 0)
 		{
-			float outEta = 0.0;
-			vec4 res = Refract(-ray.dir, info.normal, material.refractionCoefficient, outEta);
+			float etaI = 1.0;
+			float etaO = material.refractionCoefficient;
+			// Direction
+			vec4 res = Refract(-ray.dir, info.normal, material.refractionCoefficient, etaI, etaO);
 			float fresnel = res.w;
-			if (Xi(seed) <= fresnel)
+			if (Xi(seed) <= mix(1.0, fresnel, transmission.factor))
 				glassReflect = true;
+			specular.factor *= float(glassReflect);
 
 			transmission.direction = res.xyz;
+			// BRDF
+			// https://cgg.mff.cuni.cz/~jaroslav/teaching/2017-npgr010/slides/03%20-%20npgr010-2017%20-%20BRDF.pdf
+			float overlap = max(0.0, -dot(res.xyz, ray.dir));
+			float changeOfRadiance = (etaO * etaO) / (etaI * etaI);
+			// Physically-plausible Phong Distributuion
+			vec3 DPPPhong = info.material.specularColor * ((material.shininess + 2.0) / TWO_PI) * pow(overlap, material.shininess);
+			transmission.BRDF = changeOfRadiance * (1.0 - fresnel) * DPPPhong / dot(info.normal, info.lightVector);
+			transmission.factor *= float(!glassReflect);
 		}
 	}
 
-	// Temp
-	if (material.refractivity > 0.0)
-	{
-		if (glassReflect)
-			ray.dir = mix(diffuse.direction, specular.direction, material.specularity);
-		else
-			ray.dir = mix(diffuse.direction, transmission.direction, material.specularity);
-	}
-	else
-	{
-		ray.dir = mix(diffuse.direction, specular.direction, specular.factor);
-	}
-	return mix(diffuse.BRDF, specular.BRDF, specular.factor);
+	ray.dir = diffuse.direction * diffuse.factor + specular.direction * specular.factor + transmission.direction * transmission.factor;
+	return diffuse.BRDF * diffuse.factor + specular.BRDF * specular.factor + transmission.BRDF * transmission.factor;
 }
 
+struct PDFInfo
+{
+	float factor;
+	float PDF;
+};
 
 float GetPDF(inout RayTracingMaterial material)
 {
-	if (material.specularity == 1.0)
-	{
-		return 1.0;
-	}
-	return INVPI;
+	PDFInfo diffuse = PDFInfo(
+		1.0 - material.specularity,
+		INVPI
+	);
+	PDFInfo specularTransmission = PDFInfo(
+		material.specularity,
+		1.0
+	);
+	return diffuse.PDF * diffuse.factor + specularTransmission.PDF * specularTransmission.factor;
 }
 
 #endif // MATERIALS_GLSL
