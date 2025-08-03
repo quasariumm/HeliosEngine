5df3c147c7ddbe0395a67711c9a2527c3509c354
Patrick Vreeburg <96523159+quasariumm@users.noreply.github.com>
2025-08-03 13:37:26 +0200

    FIX: Launcher uses libEngined.dll if named that way

--- launcher/main.cpp
+++ launcher/main.cpp
@@ -7,7 +7,9 @@ typedef int (__stdcall *engineFunc)();
 int main()
 {
     const std::wstring TMPFolder = std::filesystem::current_path().append("launcher\\cmake-build-debug");
-    const std::wstring EngineLibrary = std::filesystem::current_path().append("cmake-build-debug\\libEngine.dll");
+	std::wstring EngineLibrary = std::filesystem::current_path().append("cmake-build-debug\\libEngined.dll");
+	if (!std::filesystem::exists(EngineLibrary))
+		EngineLibrary = std::filesystem::current_path().append("cmake-build-debug\\libEngine.dll");
 
     // Try to load the engine library
     if (std::filesystem::exists(TMPFolder + L"\\libEngine.dll"))
