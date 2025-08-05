e757da1d7730afd455f66da04584d404de0661d8
Patrick Vreeburg <96523159+quasariumm@users.noreply.github.com>
2025-08-05 19:52:03 +0200

    FIX: Just a version where the engine starts up... Assimp is interesting

--- src/Assets/ModelFileHandler.cpp
+++ src/Assets/ModelFileHandler.cpp
@@ -96,24 +96,27 @@ ModelData* ModelFileHandler::LoadModel( const std::filesystem::path& modelFile )
 	if (m_loadedModels.contains(modelFile.wstring()))
 		return &m_loadedModels[modelFile.wstring()];
 
-	Assimp::Importer importer;
+	// TODO: Assimp is unusable (any function call specifically kills it)
+	// Assimp::Importer importer;
 
-	const aiScene* scene = importer.ReadFile(modelFile.string().c_str(),
-		aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
-
-	if (!scene
-		|| scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE
-		|| !scene->mRootNode)
-	{
-		DebugLog(LogSeverity::SEVERE, std::format(L"Assimp error {0}.", STR_TO_WSTR(importer.GetErrorString()).c_str()));
-		return nullptr;
-	}
-
-	m_loadedModels[modelFile.wstring()] = {};
-
-	ProcessNode(scene->mRootNode, scene, m_loadedModels[modelFile.wstring()].meshes, modelFile.parent_path());
-
-	return &m_loadedModels[modelFile.wstring()];
+	return nullptr;
+	//
+	// const aiScene* scene = importer.ReadFile(modelFile.string().c_str(),
+	// 	aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
+	//
+	// if (!scene
+	// 	|| scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE
+	// 	|| !scene->mRootNode)
+	// {
+	// 	DebugLog(LogSeverity::SEVERE, std::format(L"Assimp error {0}.", STR_TO_WSTR(importer.GetErrorString()).c_str()));
+	// 	return nullptr;
+	// }
+	//
+	// m_loadedModels[modelFile.wstring()] = {};
+	//
+	// ProcessNode(scene->mRootNode, scene, m_loadedModels[modelFile.wstring()].meshes, modelFile.parent_path());
+	//
+	// return &m_loadedModels[modelFile.wstring()];
 }
 
 }
